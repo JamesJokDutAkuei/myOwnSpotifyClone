9ba7905ae10cda153bd5bec1c8b4942706a0a3e0
James Jok Dut Akuei <jok@192.168.1.104>
2025-02-11 01:32:27 +0200

    spotify clone

---
+++ spotifyClone/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        myGray: '#2A2A2A',
+        whiteGray: '#B3B3B3',
+      },
+    },
+  },
+  plugins: [],
+}
+
